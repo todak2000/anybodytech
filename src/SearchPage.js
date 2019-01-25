7584913415bec64ff018c9364f874a49c278ad38
MAC <mac@MACs-MBP.lan>
2019-01-25 12:42:31 +0100

    chika commit

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import Workzone from './components/Workzone';
 
 
 
@@ -16,16 +16,30 @@ class SearchPage extends Component {
         };
 
     }
+    shouldComponentUpdate(nextProps, nextState) {
+        if (this.state.transcript !== nextState.transcript && nextState.transcript !== '') {
+          
+        }
+    
+        return true;
+      }
     render() {
         return (
         <div className='body2'>
+        
+        
+              
+           
         <div className="circle2">
             <div className="app__results">
-            <h4 style={{textAlign:"center"}} className='result-item__definition'>Search Result for </h4>
-            <h3 style={{fontSize:16,textTransform:"uppercase", marginTop:0,fontFamily:'Montserrat-Bold'}}>{this.props.transcript}</h3>
+            <div style={{flexDirection:"row"}}>
+            <button style={{borderRadius:"50%",width:40,height:40, marginTop:'5%',color:'#122C3B'}} onClick={this.navigateBack}>Back</button><span>
+            <h4 style={{textAlign:"center"}} className='result-item__definition'>Search Result for </h4></span>
+            <h3 key="final" style={{fontSize:16,textTransform:"uppercase", marginTop:0,fontFamily:'Montserrat-Bold'}}>{this.props.transcript}</h3>
+            </div>
             <li>
-                <div style={{textAlign:'left'}}>
-                <img  style={{borderRadius:'100%', width:60, height:60, float:"left"}} src={this.props.location.state.results[0].img_src} alt=''/><span>
+            <div style={{textAlign:'left'}}>
+            <img  style={{borderRadius:'100%', width:60, height:60, float:"left"}} src={this.props.location.state.results[0].img_src} alt=''/><span>
                 <h3 className="result-item__definition">{this.props.location.state.results[0].profile}</h3> </span>
 
                 <img  style={{borderRadius:'100%', width:60, height:60, float:"left"}} src={this.props.location.state.results[1].img_src} alt=''/><span>
